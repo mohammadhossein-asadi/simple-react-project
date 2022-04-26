5dfa760327d06bde4e72e9e480488cd931f690e5
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-04-26 14:57:13 +0430

    commit change and add route

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+import { Route } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetups";
+import FavoritesPage from "./pages/Favorites";
+
 function App() {
   return <div></div>;
 }
