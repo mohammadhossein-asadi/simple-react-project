80f938a3dee186415fb107debbd403be4c0b38fc
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-04-26 15:38:16 +0430

    change

--- src/App.js
+++ src/App.js
@@ -5,7 +5,19 @@ import NewMeetupPage from "./pages/NewMeetups";
 import FavoritesPage from "./pages/Favorites";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Route path="/">
+        <AllMeetupsPage />
+      </Route>
+      <Route path="/new-meetup">
+        <NewMeetupPage />
+      </Route>
+      <Route path="/favorites">
+        <FavoritesPage />
+      </Route>
+    </div>
+  );
 }
 
 export default App;
