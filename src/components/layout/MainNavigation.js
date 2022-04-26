63479c2b5f3b8ac4581ba43cc223a6d8df0dc3e3
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-04-26 16:16:29 +0430

    change

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
 
+import classes from "../layout/MainNavigation.module.css";
+
 function MainNavigation() {
   return (
-    <header>
+    <header className={classes.header}>
       <div>React Meetups</div>
       <nav>
         <ul>
